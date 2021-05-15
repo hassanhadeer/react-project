c35420b59567dd308f28c7432f62f31f7c7cd2b6
Hassan Abuhadeer <hassanhadeer.canada@gmail.com>
2021-05-15 01:15:02 -0400

    SocialIcons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link, withRouter } from "react-router-dom"
 import './Footer.css'
+import Facebook from '../images/Facebook.png'
+import LinkedIn from '../images/LinkedIn.png'
+import Twitter from '../images/Twitter.png'
+
 
 export default function Footer() {
   return (
@@ -10,6 +14,11 @@ export default function Footer() {
         <Link class="Fot-nav-link" to="/About">About</Link>
         <Link class="Fot-nav-link" to="/Careers">Careers</Link>
         <Link class="Fot-nav-link" to="/Contact">Contact us</Link>
+      </div>
+      <div >
+        <a href="https://ca.linkedin.com/" target="_blank"><img class="SocialIcons" src={LinkedIn} /></a>
+        <a href="https://www.facebook.com/" target="_blank"><img class="SocialIcons" src={Facebook} /></a>
+        <a href="https://twitter.com/?lang=en-ca" target="_blank"><img class="SocialIcons" src={Twitter} /></a>
       </div>
       <div class="Copyright">
         <a class="Fot-nav-link">© 2021 Hassan Hadeer, React project!</a>
